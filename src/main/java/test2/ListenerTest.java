82b58456015a1724b0ab3f74df0911641396d5ac
Rostmp <84409690+Rostmp@users.noreply.github.com>
2022-08-20 14:04:01 +0300

    fix: refactor Listener class

--- src/main/java/test2/ListenerTest.java
+++ src/main/java/test2/ListenerTest.java
@@ -2,19 +2,17 @@ package test2;
 
 import com.google.auto.service.AutoService;
 import org.testng.ITestListener;
-import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 
 import java.lang.reflect.Method;
 
 @AutoService(ITestListener.class)
-public class MyListener implements ITestListener {
+public class ListenerTest implements ITestListener {
 
     @Override
     public void onTestSuccess(ITestResult result) {
-        ITestNGMethod method = result.getMethod();
         try {
-            Method methodReal = method.getRealClass().getMethod(result.getName());
+            Method methodReal = result.getMethod().getRealClass().getMethod(result.getName());
             TestMethodInfo annotations = methodReal.getAnnotation(TestMethodInfo.class);
             System.out.println(annotations.priority());
             System.out.println(annotations.author());
